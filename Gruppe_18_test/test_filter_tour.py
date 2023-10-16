49e0d2868ff1d176438110cb822ec4963346ce6e
kosovarei <111733299+kosovarei@users.noreply.github.com>
2023-10-16 20:10:56 +0200

    changed the test_filter_tour file to approval testing

--- Gruppe_18_test/test_filter_tour.py
+++ Gruppe_18_test/test_filter_tour.py
@@ -3,32 +3,19 @@ import json
 import pytest
 from Gruppe_18_src.Tour import Tour
 from Gruppe_18_src.Filter_tour import *
+from approvaltests import verify
 
 
 def test_all_tours_from_file_append_to_list():
-    tours = read_tours_from_file_add_to_list("tour.json")
-    with open("tour.json") as file:
-        data1 = json.load(file)
-    assert data1 == tours
+    tours = read_tours_from_file_add_to_list("filter_test.json")
+    verify(tours)
 
 
 def test_filter_tours_by_location_as_expected():
-    test = []
-    filter = filter_tour_by_location(read_tours_from_file_add_to_list("tour.json"), "Paris")
-    with open("tour.json") as file:
-        data1 = json.load(file)
-        for data in data1:
-            if data['destination'] == 'Paris':
-                test.append(data)
-    assert filter == test
+    filtered = filter_tour_by_location(read_tours_from_file_add_to_list("filter_test.json"), "London")
+    verify(filtered)
 
 
 def test_filter_tours_by_price_as_expected():
-    test = []
-    filter = filter_tour_by_price(read_tours_from_file_add_to_list("tour.json"), 100, 0)
-    with open("tour.json") as file:
-        data1 = json.load(file)
-        for data in data1:
-            if 0 <= data['cost'] <= 100:
-                test.append(data)
-    assert filter == test
+    filtered = filter_tour_by_price(read_tours_from_file_add_to_list("filter_test.json"), 150, 0)
+    verify(filtered)
