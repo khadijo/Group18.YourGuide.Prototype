2e8f12f11d9ada117e6ad8798669fd7a33830a8d
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2023-10-17 10:23:42 +0200

    Implement MVC Architecture
    This commit includes the following changes:
    - Created model classes to represent data (e.g., Account, Tour).
    - Implemented controller classes for business logic (e.g., AccountController, TourController).
    - Created repository classes for data collection and storage (e.g., AccountRepository, TourRepository).
    - Organized the project directory according to the MVC structure.
    
    This structure helps to separate concerns in the project and provides a more organized approach to development.

--- Gruppe_18/src/main.py
+++ Gruppe_18/src/main.py
@@ -1,5 +1,6 @@
-from Gruppe_18_src.Account import Account
-from Gruppe_18_src.Tour import Tour
+from Gruppe_18.src.Model.Account import Account
+from Gruppe_18.src.Model.Tour import Tour
+from Gruppe_18.src.Repository.AccountRepository import AccountRepository
 
 account = Account("k", "kjwhd73eh", 48282234, "kariMarie@gmail.com")
 account2 = Account("m", "89283447", 4839759793, "koiejewjre@gmail.com")
@@ -22,6 +23,6 @@ tour2 = Tour("Italy",
         20)
 
 
-tour.save_to_json("tour.json")
-tour2.save_to_json("tour.json")
-print(tour.check_if_tour_is_saved(2))
+account_repository = AccountRepository()
+account = Account("username", "password", 12345678, "user_@gmail.com")
+account_repository.save_to_json(account)
