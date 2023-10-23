2c454ef46fe045e4f11057fff0bf5a7c326430ab
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2023-10-23 10:36:46 +0200

    I adapted the tests in 'test_Tour' to now use 'tourRepository' instead of accessing the entities directly to utilize the methods. Consequently, I relocated all the methods from the entity into 'tour'.

--- Gruppe_18/src/main/repository/TourRepository.py
+++ Gruppe_18/src/main/repository/TourRepository.py
@@ -1,17 +1,31 @@
+import json
+
 from Gruppe_18.src.main.repository.JSONRepository import JSONRepository
+
 
 # list_with_tours =
 
-class TourRepository (JSONRepository):
+class TourRepository(JSONRepository):
 
-    def filter_tour_by_location(tour_list, destination):
+    def book_tour(self, entity):
+        if not entity.booked >= entity.max_travelers:
+            entity.booked += 1
+            return True
+        else:
+            return False
+
+    def get_tour_description(self, entity):
+        return f"This tour will take you to {entity.destination} for {entity.duration} hours, and is " \
+               f"offered in {entity.language}"
+
+    def filter_tour_by_location(self, tour_list, destination):
         filtered_tours = []
         for tour in tour_list:
             if tour['destination'] == destination:
                 filtered_tours.append(tour)
         return filtered_tours
 
-    def filter_tour_by_price(tour_list, max_price, min_price):
+    def filter_tour_by_price(self, tour_list, max_price, min_price):
         filtered_tours = []
         for tour in tour_list:
             if min_price <= tour['cost'] <= max_price:
