037e3e6eb11a6df664a881b63b6ed9648505af17
kosovarei <111733299+kosovarei@users.noreply.github.com>
2023-10-23 09:22:52 +0200

    fixed the datatypes and tested returning a row that i added

--- Gruppe_18/src/main/database/SQLite.py
+++ Gruppe_18/src/main/database/SQLite.py
@@ -1,12 +1,31 @@
 import sqlite3
-import sys
 
 connection = sqlite3.connect("YourGuide.db")
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE tour (title String, destination String, duration Float, cost Float,"
-               "pictureURL String, language String, MAX_travelers Integer, Booked Integer, Tour_ID String)")
+cursor.execute("CREATE TABLE tour (title TEXT, destination TEXT, duration REAL, cost REAL, "
+               "pictureURL TEXT, language TEXT, MAX_travelers INTEGER, Booked INTEGER, Tour_ID TEXT)")
 
-cursor.execute("CREATE TABLE Account (username String, password String, phonen, emailAdress String, account_id Integer)")
+cursor.execute("CREATE TABLE Account (username TEXT, password TEXT, phonen TEXT, emailAdress TEXT, account_id Integer)")
+
+cursor.execute("INSERT INTO tour (title, destination, duration, cost, pictureURL, "
+               "language, MAX_travelers, Booked, Tour_ID) "
+               "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+               ("Tour Title", "Destination", 7.5, 500.0, "example.jpg", "English", 20, 5, "12345"))
+
+
+connection.commit()
+
+# Now, let's retrieve the inserted row as a dictionary
+cursor.execute("SELECT * FROM tour WHERE Tour_ID = ?", ("12345",))
+row = cursor.fetchone()
+
+print(row)
+
+# Close the cursor and the connection
+cursor.close()
+connection.close()
+
+
 
 
