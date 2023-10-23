24dd05029cafe034cdc735d93d847eab224f3c8f
kosovarei <111733299+kosovarei@users.noreply.github.com>
2023-10-23 09:57:45 +0200

    added primary key to creating_tables

--- Gruppe_18/src/main/database/creating_tables.py
+++ Gruppe_18/src/main/database/creating_tables.py
@@ -5,9 +5,9 @@ connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
 cursor.execute("CREATE TABLE tour (title TEXT, destination TEXT, duration REAL, cost REAL, "
-               "pictureURL TEXT, language TEXT, MAX_travelers INTEGER, Booked INTEGER, Tour_ID TEXT)")
+               "pictureURL TEXT, language TEXT, MAX_travelers INTEGER, Booked INTEGER, Tour_ID TEXT PRIMARY KEY)")
 
-cursor.execute("CREATE TABLE Account (username TEXT, password TEXT, phonen TEXT, emailAdress TEXT, account_id Integer)")
+cursor.execute("CREATE TABLE Account (username TEXT, password TEXT, phonen TEXT, emailAdress TEXT, account_id Integer PRIMARY KEY)")
 
 
 
