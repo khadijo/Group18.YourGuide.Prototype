65b606e2477a0fc088d5f026ec86081f3e049cb4
khad1 <khadij011@outlook.com>
2023-10-24 12:00:44 +0200

    Deleted tests data values, add new values with more information

--- Gruppe_18/src/main/main.py
+++ Gruppe_18/src/main/main.py
@@ -13,23 +13,30 @@ a = Account(
         12345678,
         "user_@gmail.com"
     )
-tour = Tour("Welcome to Dubai",
-        datetime.date(2020, 10, 15),
-        "Dubai",
-        4,
-        255,
-        15,"English",
-        "https://www.hdwallpaper.nu/wp-content/uploads/2015/05/colosseum-1436103.jpg")
+tour = Tour("Welcome to Lofoten",
+        datetime.date(2024, 6, 25),
+        "Lofoten, Norwat",
+        3,
+        3674,
+        20,"English",
+        "https://www.thonhotels.no/siteassets/artikler/lofoten/nordlys-lofoten-1.jpg")
 
 
-new_tour = tour_repository.create_tour(tour)
+
+# new_tour = tour_repository.create_tour(tour)
 
 
 #result = tour_repository.book_tour(new_tour)
 #result1 = tour_repository.book_tour(new_tour)
 
-# tour_repository.delete_tour("25129e5f-3599-40be-be6a-07b87ba7762e")
-print(tour_repository.get_tour_description("67eed256-fef7-4314-8900-b543cf5a3ecd"))
+tour_repository.delete_tour("25129e5f-3599-40be-be6a-07b87ba7762e")
+tour_repository.delete_tour("009ea0dc-5fae-4538-8c6c-e439fa876e7c")
+tour_repository.delete_tour("e9e2fa19-09c7-4fcb-8b86-bfbaefdf236b")
+tour_repository.delete_tour("0c784c92-9e5b-42d3-91d2-21c21eafb6f6")
+tour_repository.delete_tour("42018dd6-5b95-4852-9016-7f63e9856136")
+
+# Create new tours
+# new_tour = tour_repository.create_tour(tour)
 
 
 session.commit()
