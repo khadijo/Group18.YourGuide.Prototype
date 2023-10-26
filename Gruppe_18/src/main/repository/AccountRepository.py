719acbe0a54cf9f9cdf7085bb6e001e8cefe4fee
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2023-10-26 10:57:18 +0200

    The last commit was missing create_account method i AccountRepository and the app.py was missing an AccountRepository with session. now the code is working as expected.

--- Gruppe_18/src/main/repository/AccountRepository.py
+++ Gruppe_18/src/main/repository/AccountRepository.py
@@ -2,13 +2,15 @@ from Gruppe_18.src.main.repository.JSONRepository import JSONRepository
 from Gruppe_18.src.main.model.models import Account
 
 
-class AccountRepository (JSONRepository):
+class AccountRepository(JSONRepository):
+
+    def __init__(self, session):
+        self.session = session
+
     def delete_account(self, session, entity):
-        # Søk etter kontoen som skal slettes
         account_to_delete = session.query(Account).filter_by(username=entity.username).first()
 
         if account_to_delete:
-            # Hvis kontoen ble funnet, slett den fra databasen
             session.delete(account_to_delete)
             session.commit()
             return True
@@ -40,9 +42,19 @@ class AccountRepository (JSONRepository):
 
         return False
 '''
+
     # needs to be updated after database is implemented
 
     def successful_registration(self, entity, io_stream):
         self.save_to_stream(entity, io_stream)
         return True
 
+    def create_account(self, entity):
+        account = Account(username=entity.username,
+                          password=entity.password,
+                          phoneNumber=entity.phoneNumber,
+                          emailAddress=entity.emailAddress)
+
+        self.session.add(account)
+        self.session.commit()
+        return account
