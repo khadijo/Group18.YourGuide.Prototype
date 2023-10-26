2b1fa64f4eed8391be163e5fbe483ba2f205de43
kosovarei <111733299+kosovarei@users.noreply.github.com>
2023-10-26 11:48:38 +0200

    nothing really

--- Gruppe_18/test/test_filter_tour.py
+++ Gruppe_18/test/test_filter_tour.py
@@ -3,23 +3,21 @@ from approvaltests import verify
 from Gruppe_18.src.main.repository.TourRepository import TourRepository
 from Gruppe_18.test.database.database_handler import get_session
 
+
 @pytest.fixture
 def tour_re():
-    rep = TourRepository(get_session())
-    return rep
-
-
-
-
+    return TourRepository(get_session())
 
 
 def test_if_reading_all_tours_from_database_is_as_expected(tour_re):
     all_tours = tour_re.get_all_tours()
     verify(all_tours)
 
+
 def test_if_a_spesific_tour_can_be_returned_from_database(tour_re):
     tour = tour_re.get_spesific_tour("id")
     assert tour == ""
+
 
 def test_if_filter_tours_by_location_is_as_expected(tour_re):
     filtered_tours = tour_re.filter_tour_by_location("London")
