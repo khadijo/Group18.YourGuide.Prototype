25c316ff2589b4fc176f765b691a96c924e75f56
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2023-10-29 19:07:08 +0100

    added a method to register a user to a tour

--- Gruppe_18/src/main/repository/AccountRepository.py
+++ Gruppe_18/src/main/repository/AccountRepository.py
@@ -1,8 +1,11 @@
 from Gruppe_18.src.main.repository.JSONRepository import JSONRepository
-from Gruppe_18.src.main.model.models import Account
+from Gruppe_18.src.main.model.models import Account, Tour, tour_account_association
 
 
 class AccountRepository (JSONRepository):
+    def __init__(self, session):
+        self.session = session
+
     def delete_account(self, session, entity):
         # Søk etter kontoen som skal slettes
         account_to_delete = session.query(Account).filter_by(username=entity.username).first()
@@ -45,4 +48,21 @@ class AccountRepository (JSONRepository):
     def successful_registration(self, entity, io_stream):
         self.save_to_stream(entity, io_stream)
         return True
+
+
+    def account_register_to_tour(self, tour_id, user_id):
+        tour = self.session.query(Tour).filter_by(tour_id=tour_id).first()
+        user = self.session.query(Account).filter_by(account_id=user_id).first()
+
+        if tour is not None and user is not None:
+            tour_account_assoc_obj = tour_account_association.insert().values(
+                    tour_id=tour_id,
+                    account_id=user_id
+                )
+            self.session.execute(tour_account_assoc_obj)
+            self.session.commit()
+
+        else:
+            print("Tur eller bruker ble ikke funnet.")
+
 
