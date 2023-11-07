e664c5da4b4ce79ec7450a18ba0469e012b6346f
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2023-11-07 10:18:18 +0100

    Made an AccountController, so that I can move all the methods in app.py there and clean app.py

---
+++ Gruppe_18/src/main/controller/AccountController.py
@@ -0,0 +1,11 @@
+from flask import request, redirect, render_template, flash, url_for
+from flask_login import login_user
+
+from Gruppe_18.src.main.main import session
+from Gruppe_18.src.main.model.models import Account
+
+
+class AccountController:
+    def __init__(self, account_repository):
+        self.account_repository = account_repository
+
